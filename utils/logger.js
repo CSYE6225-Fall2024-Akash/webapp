e903b59c22a539b4e8b5e01d4850a1c64496b719
AkashArumugam6 <venkatachalapathy.a@northeastern.edu>
2024-10-31 10:43:35 -0400

    Updated logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,6 +1,14 @@
 const winston = require('winston');
 const CloudWatchTransport = require('winston-cloudwatch');
 
+
+var options = {
+    file: {
+      level: 'info',
+      filename: `/opt/webapp/combined.log`,    
+    },
+  }
+
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
@@ -9,6 +17,7 @@ const logger = winston.createLogger({
     ),
     transports: [
         new winston.transports.Console(),
+        new winston.transports.File(options.file),
         new CloudWatchTransport({
             logGroupName: 'csye6225',
             logStreamName: 'webapp',
