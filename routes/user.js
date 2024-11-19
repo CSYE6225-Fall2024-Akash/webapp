8461d3fe6da3d48f63eaf3609e39ee78834c0687
AkashArumugam6 <venkatachalapathy.a@northeastern.edu>
2024-11-18 20:35:58 -0500

    Update route

--- routes/user.js
+++ routes/user.js
@@ -91,7 +91,7 @@ router.post('/v1/user', async (req, res) => {
             email: user.email,
             accountCreated: user.account_created,
             verificationToken: verificationToken,
-            verificationUrl: `https://${process.env.DOMAIN_NAME}/v1/verify?email=${encodeURIComponent(email)}&token=${verificationToken}`
+            verificationUrl: `http://${process.env.DOMAIN_NAME}/v1/verify?email=${encodeURIComponent(email)}&token=${verificationToken}`
         };
 
         await sns.publish({
